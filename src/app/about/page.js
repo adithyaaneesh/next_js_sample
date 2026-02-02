8b0b10d03e028a156190ee4973ed4c7143b84b15
adithyaaneesh <adithyaaneesh29@gmail.com>
2026-02-02 15:25:06 +0530

    page routing

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,14 @@
+"use client"
+import { useRouter } from "next/navigation";
 export default function AboutPage() {
-    return <h1>ABOUT US</h1>;
+    const router = useRouter();
+    return(
+    <div>
+        <h1>ABOUT US</h1>
+        <button 
+        onClick={()=>router.push("/")}
+        className="mt-4 px-4 bg-blue-500 text-white rounded"
+        >Go Home</button>
+    </div>
+    );
 }
